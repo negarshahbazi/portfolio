1b8844b0477cecf5f590d8e51675531ea19d4a37
john Doe <negarshahbazi.official@gmail.com>
2023-12-28 17:30:36 +0100

    28dec

---
+++ main.js
@@ -0,0 +1,9 @@
+// let button=document.getElementsByClassName('myButtonToUp');
+// button.addEventListener('click',()=>{
+
+// });
+document.querySelectorAll(".box").addEventListener('mouseover',()=>{
+document.querySelectorAll(".nameProgramme").style.visibility = "visible";
+
+
+})
