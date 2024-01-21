424eb77b4ea6c56a970da9faaf64d33cfa5f6170
john Doe <negarshahbazi.official@gmail.com>
2024-01-21 21:11:20 +0100

    zez

--- main.js
+++ main.js
@@ -16,3 +16,97 @@ document.querySelector(".nameProgramme1").style.visibility = "hidden";
 document.getElementsByClassName('hoverMenue').addEventListener('mouseover',()=>{
     document.getElementsByClassName('hoverMenue').classList = "nav-link active text-white opacity-50 hoverMenue" ; 
 })
+
+// 
+(function(){
+  var addEvent = function(object, type, callback) {
+      if (object == null || typeof(object) == 'undefined') return;
+      if (object.addEventListener) {
+          object.addEventListener(type, callback, false);
+      } else if (object.attachEvent) {
+          object.attachEvent("on" + type, callback);
+      } else {
+          object["on"+type] = callback;
+      }
+  };
+  function detectMouseMove() {
+    // Initial container width
+    var containerWidth = $('.container').outerWidth();
+    document.addEventListener('mousemove', function(e) {
+      $('.hover').css({
+         left:  e.pageX
+      });
+      $('.left-side').css({
+        left: e.pageX + 75
+      });
+      var rightSideLeft = containerWidth - e.pageX;
+      $('.right-side').css({
+        right: rightSideLeft
+      });
+
+    })
+  }
+  detectMouseMove();
+  
+  
+  function detectTouch() {
+    // Initial container width
+    var containerWidth = $('.container').outerWidth();
+    document.addEventListener('mousemove', function(e) {
+      $('.hover').css({
+         left:  e.pageX
+      });
+      $('.left-side').css({
+        left: e.pageX + 75
+      });
+      var rightSideLeft = containerWidth - e.pageX;
+      $('.right-side').css({
+        right: rightSideLeft
+      });
+    })
+  }
+  detectMouseMove();
+  
+  function touchMove(){
+    // Initial container width
+    var containerWidth = $('.container').outerWidth();
+    document.addEventListener('touchmove', function(e) {
+      $('.hover').css({
+         left:  e.pageX
+      });
+      $('.left-side').css({
+        left: e.pageX + 75
+      });
+      var rightSideLeft = containerWidth - e.pageX;
+      $('.right-side').css({
+        right: rightSideLeft
+      });
+
+    })
+  }
+  
+  touchMove();
+  
+  function detectInput(inputType){
+    // Detect container width after resizing
+    addEvent(window, "resize", function(event) {
+      var containerWidth = $('.container').outerWidth();
+      $(document).bind(inputType, function(e){
+        $('.hover').css({
+           left:  e.pageX
+        });
+        $('.left-side').css({
+          left: e.pageX + 75
+        });
+        var rightSideLeft = containerWidth - e.pageX;
+        $('.right-side').css({
+          right: rightSideLeft
+        });
+      });
+    });
+  }
+  
+  detectInput('mousemove');
+  detectInput('touchmove');
+  
+})();
