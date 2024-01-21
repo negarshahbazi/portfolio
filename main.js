9ef419a9a4eb6ad4483ab4f77db898dc5db6d178
john Doe <negarshahbazi.official@gmail.com>
2024-01-21 19:52:30 +0100

    rdr

--- main.js
+++ main.js
@@ -11,3 +11,8 @@ document.querySelector(".nameProgramme1").style.visibility = "hidden";
 
 
 })
+
+
+document.getElementsByClassName('hoverMenue').addEventListener('mouseover',()=>{
+    document.getElementsByClassName('hoverMenue').classList = "nav-link active text-white opacity-50 hoverMenue" ; 
+})
